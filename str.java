2bac87d276204839b26b160f02874a8f73185ac7
Nazik007 <99899134+Nazik007@users.noreply.github.com>
2022-05-20 21:44:15 +0300

    Create str.java

---
+++ str.java
@@ -0,0 +1,9 @@
+public class Strings {
+    public static void main(String[] args) {
+        String str1 = "\nмоя строка 1";
+        String str2 = "\tмоя строка 2";
+        // String str3 = "\aмоя строка 3;
+
+        System.out.println(str1 + str2);
+    }
+}
